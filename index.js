3398b147412072c8c7be090e188bd0bcc7e9aff2
Madan Mohan Singh <madansingh18@gmail.com>
2015-04-12 20:13:18 +0530

    5

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
